3df397dec8fc1377053c8610a2d7b925cf5d71e0
Pavel Nikiforov <pavel.vlad.nikiforov@gmail.com>
2026-06-01 23:58:32 +0300

    Fix Maven plugin in ci.yml

--- src/main/java/com/innowise/authservice/config/AdminProperties.java
+++ src/main/java/com/innowise/authservice/config/AdminProperties.java
@@ -4,6 +4,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "admin")
-@Validated
 public record AdminProperties(String email, String password, String userId) {
 }
